a0d11324b8f9971e500656d6f976576884e8bf47
EliseoLeonenco <eliseoleonenco02@gmail.com>
2023-11-30 22:54:40 +0000

    quit button functionality

--- app/src/main/java/com/example/newsapiproject/MainActivity.kt
+++ app/src/main/java/com/example/newsapiproject/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.newsapiproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var quit_btn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        quit_btn = findViewById(R.id.quit_btn)
+
+        //quit button exits the application
+        quit_btn.setOnClickListener {
+            finish()
+        }
     }
 }
