f9f61b022495feba898149879aa5bbf12c0e22cc
EliseoLeonenco <eliseoleonenco02@gmail.com>
2023-11-30 23:00:27 +0000

    news search activity add

---
+++ app/src/main/java/com/example/newsapiproject/NewsSearchActivity.kt
@@ -0,0 +1,11 @@
+package com.example.newsapiproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_search)
+    }
+}
